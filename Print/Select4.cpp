1da552bc9b149c7641849fdee7f4947718dd1c04
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-05-04 15:12:04 +0300

    Update Select4.cpp

--- Print/Select4.cpp
+++ Print/Select4.cpp
@@ -5,18 +5,17 @@
 #include "../Header/Blowfish.h"
 #include "../Header/print.h"
 
-using namespace std;
 
 void print4 (uchar *input)
 {
-	Blowfish *B = new(nothrow)Blowfish;
-	Spritz *S = new(nothrow)Spritz;
+	Blowfish *B = new(std::nothrow)Blowfish;
+	Spritz *S = new(std::nothrow)Spritz;
 
 	uint32_t L, R;
 
-	uchar *cipher = new(nothrow)uint8_t[strlen((const char*)input)];
+	uchar *cipher = new(std::nothrow)uint8_t[strlen((const char*)input)];
 	int len_Spritz_key = strlen((const char*)S->Spritz_key());
-	char *key_temp_Spritz = new(nothrow)char[len_Spritz_key];
+	char *key_temp_Spritz = new(std::nothrow)char[len_Spritz_key];
 	memcpy (key_temp_Spritz, S->Spritz_key(), len_Spritz_key);
 
 	/* Performing Spritz */
@@ -24,7 +23,7 @@ void print4 (uchar *input)
 	S->Encrypt ((uchar*)input, key_temp_Spritz, cipher);
 	
 	int len_Blowfish_key = strlen((const char*)B->Blowfish_key());
-	uchar *key_temp_Blowfish = new(nothrow)uchar[len_Blowfish_key];
+	uchar *key_temp_Blowfish = new(std::nothrow)uchar[len_Blowfish_key];
 	memcpy (key_temp_Blowfish, B->Blowfish_key(), len_Blowfish_key);
 
 	B->crack_text (cipher, &L, &R, strlen((const char*)cipher));
