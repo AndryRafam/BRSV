df57973e7e21ddcc0231bac939a996cc15dd0fe2
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-05-04 15:10:17 +0300

    Update Select1.cpp

--- Print/Select1.cpp
+++ Print/Select1.cpp
@@ -5,18 +5,17 @@
 #include "../Header/Blowfish.h"
 #include "../Header/print.h"
 
-using namespace std;
 
 void print1 (uchar *input)
 {
-	Blowfish *B = new(nothrow)Blowfish;
-	RC4A *RA = new(nothrow)RC4A;
+	Blowfish *B = new(std::nothrow)Blowfish;
+	RC4A *RA = new(std::nothrow)RC4A;
 		
 	uint32_t L, R;
 	
-	uchar *cipher = new(nothrow)uint8_t[strlen((const char*)input)];
+	uchar *cipher = new(std::nothrow)uint8_t[strlen((const char*)input)];
 	int len_RC4A_key = strlen((const char*)RA->RC4A_key());
-	char *key_temp_RC4A = new(nothrow)char[len_RC4A_key];
+	char *key_temp_RC4A = new(std::nothrow)char[len_RC4A_key];
 	memcpy (key_temp_RC4A, RA->RC4A_key(), len_RC4A_key);
 
 	/* Performing RC4A */
@@ -24,7 +23,7 @@ void print1 (uchar *input)
 	RA->Encrypt ((uchar*)input, key_temp_RC4A, cipher);
 	
 	int len_Blowfish_key = strlen((const char*)B->Blowfish_key());
-	uchar *key_temp_Blowfish = new(nothrow)uchar[len_Blowfish_key];
+	uchar *key_temp_Blowfish = new(std::nothrow)uchar[len_Blowfish_key];
 	memcpy (key_temp_Blowfish, B->Blowfish_key(), len_Blowfish_key);
 
 	/* Performing Blowfish */
