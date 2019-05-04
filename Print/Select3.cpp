524a47cc6c79f130a80cdb8d0429bff2ec6bfc1f
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-05-04 15:11:31 +0300

    Update Select3.cpp

--- Print/Select3.cpp
+++ Print/Select3.cpp
@@ -5,19 +5,18 @@
 #include "../Header/Blowfish.h"
 #include "../Header/print.h"
 
-using namespace std;
 
 void print3 (uchar *input)
 {
 
-	Blowfish *B = new(nothrow)Blowfish;
-	VMPC *V = new(nothrow)VMPC;
+	Blowfish *B = new(std::nothrow)Blowfish;
+	VMPC *V = new(std::nothrow)VMPC;
 		
 	uint32_t L, R;
 	
-	uchar *cipher = new(nothrow)uint8_t[strlen((const char*)input)];
+	uchar *cipher = new(std::nothrow)uint8_t[strlen((const char*)input)];
 	int len_VMPC_key = strlen((const char*)V->VMPC_key());
-	char *key_temp_VMPC = new(nothrow)char[len_VMPC_key];
+	char *key_temp_VMPC = new(std::nothrow)char[len_VMPC_key];
 	memcpy (key_temp_VMPC, V->VMPC_key(), len_VMPC_key);
 
 	/* Performing VMPC */
@@ -25,7 +24,7 @@ void print3 (uchar *input)
 	V->Encrypt ((uchar*)input, key_temp_VMPC, cipher);
 	
 	int len_Blowfish_key = strlen((const char*)B->Blowfish_key());
-	uchar *key_temp_Blowfish = new(nothrow)uchar[len_Blowfish_key];
+	uchar *key_temp_Blowfish = new(std::nothrow)uchar[len_Blowfish_key];
 	memcpy (key_temp_Blowfish, B->Blowfish_key(), len_Blowfish_key);
 
 	/* Performing Blowfish */
