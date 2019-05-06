2bdcad14daa891faa9c0d4432faa50d52324cbc5
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-05-06 15:44:07 +0300

    Add files via upload

---
+++ Print/Salt.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <new>
+#include <ctime>
+#include "../Header/print.h"
+
+typedef unsigned char uchar;
+
+uchar *Salt() {
+	
+	uchar *string = new(std::nothrow)uchar[6];
+	srand(time(0));
+	int i = 0;
+
+	while (i < 6) {
+		int random = rand() % 2;
+		int key = rand() % 26;
+
+		if (random == 0)
+			string[i] = (char)((key-65) % 26 + 65);
+		else if (random == 1)
+			string[i] = (char)((key-97) % 26 + 97);
+		i += 1;
+	}
+	return string;
+}
