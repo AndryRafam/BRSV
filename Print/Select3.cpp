d8e07e1325bc43062601da7bb7e7d1b15e84fffb
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-07-12 10:46:06 +0300

    Update Select3.cpp

--- Print/Select3.cpp
+++ Print/Select3.cpp
@@ -11,9 +11,9 @@ void print3 (uchar *input)
 
 	Blowfish *B = new(std::nothrow)Blowfish;
 	VMPC *V = new(std::nothrow)VMPC;
-		
+
 	uint32_t L, R;
-	
+
 	uchar *cipher = new(std::nothrow)uint8_t[strlen((const char*)input)];
 	int len_VMPC_key = strlen((const char*)V->VMPC_key());
 	char *key_temp_VMPC = new(std::nothrow)char[len_VMPC_key];
@@ -22,7 +22,7 @@ void print3 (uchar *input)
 	/* Performing VMPC */
 
 	V->Encrypt ((uchar*)input, key_temp_VMPC, cipher);
-	
+
 	int len_Blowfish_key = strlen((const char*)B->Blowfish_key());
 	uchar *key_temp_Blowfish = new(std::nothrow)uchar[len_Blowfish_key];
 	memcpy (key_temp_Blowfish, B->Blowfish_key(), len_Blowfish_key);
@@ -31,8 +31,10 @@ void print3 (uchar *input)
 	B->Blowfish_Init (key_temp_Blowfish, len_Blowfish_key);
 	B->crack_text (cipher, &L, &R, strlen((const char*)cipher));
 	B->Blowfish_Encipher (&L, &R);
-	fprintf(stdout,"%08x %08x\n\n\n", R, L); // Print the right part first, then the left part of the encrypted message
-
+	fprintf(stdout,"\n\n");
+	fprintf(stdout,"\t\t\t\t%08X\n ", R); // Print the right part first, then the left part of the encrypted message
+	fprintf(stdout,"\t\t\t\t%08X\n\n\n ", L);
+	
 	delete [ ] key_temp_VMPC;
 	delete [ ] key_temp_Blowfish;
 	delete [ ] cipher;
