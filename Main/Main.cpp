041b3d1c054fd7d25613e6a02052809b250c4267
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-07-12 10:48:14 +0300

    Update Main.cpp

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,4 +1,4 @@
-/* Andry RAFAM ANDRIANJAFY - Eagle April 2019 (Updated May 2019)
+/* Andry RAFAM ANDRIANJAFY - SR-71
 
 OS : Ubuntu 16.04 LTS
 
@@ -23,42 +23,38 @@ int main (void) {
 	uchar *salt_message = new(std::nothrow)uchar[6+MAX];
 
 	std::cout << Cyan << "\n\t*******************************************************************" << std::endl;
-	std::cout << Red << "\n\t\t[ Beldorion (Andry Rafam Andrianjafy) ]" << std::endl;
+	std::cout << Red << "\n\t\t\t[ SR-71 (Andry Rafam Andrianjafy) ]" << std::endl;
 	std::cout << Cyan << "\n\t*******************************************************************" << std::endl;
 	std::cout << Reset;
 
-	std::cout << Yellow << "\n\n\t [ Input a clear message ] → "; std::cout << Reset;
+	std::cout << Yellow << "\n\n\t\t [ Input a clear message ] → "; std::cout << Reset;
 	std::cin.get((char*)clear_message,MAX);
 	std::random_shuffle(clear_message,clear_message+strlen((const char*)clear_message)); // Shuffle the clear message
-	std::cout << Yellow << "\n\n\t [ Shuffled clear message ] → " << Reset << clear_message << std::endl;
+	std::cout << Yellow << "\n\n\t\t [ Shuffled clear message ] → " << Reset << clear_message << std::endl;
 	OTP (clear_message);
-	std::cout << "\n\n\t << APPLYING OTP ... DONE >> " << std:: endl;
+	std::cout << "\n\n\t\t << APPLYING OTP ... DONE >> " << std:: endl;
 	strcpy((char*)salt_message,(char*)OTP(clear_message));
 	strcat((char*)salt_message,(char*)Salt()); // Shuffle the salt message
 	std::random_shuffle(salt_message,salt_message+strlen((const char*)salt_message));
-	std::cout << "\n\n\t << SALTING MESSAGE ... DONE >> " << std::endl;
+	std::cout << "\n\n\t\t << SALTING MESSAGE ... DONE >> " << std::endl;
 
 	srand(time(NULL));
 	int random = rand() % 4;
 
 	if (random == 0) {
-		std::cout << Green << "\n\n\t [ ENCRYPTED MESSAGE (Blowfish_RC4A) ] → "; std::cout << Reset;
-		std::cout << Cyan;
+		std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE (Blowfish_RC4A) ]"; std::cout << Reset;
 		print1 (salt_message); std::cout << Reset;
 	}
 	else if (random == 1) {
-		std::cout << Green << "\n\n\t [ ENCRYPTED MESSAGE (Blowfish_RC4plus) ] → "; std::cout << Reset;
-		std::cout << Cyan;
+		std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE (Blowfish_RC4plus) ]"; std::cout << Reset;
 		print2 (salt_message); std::cout << Reset;
 	}
 	else if (random == 2) {
-		std::cout << Green << "\n\n\t [ ENCRYPTED MESSAGE (Blowfish_VMPC) ] → "; std::cout << Reset;
-		std::cout << Cyan;
+		std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE (Blowfish_VMPC) ]"; std::cout << Reset;
 		print3 (salt_message); std::cout << Reset;
 	}
 	else if (random == 3) {
-		std::cout << Green << "\n\n\t [ ENCRYPTED MESSAGE (Blowfish_Spritz) ] → "; std::cout << Reset;
-		std::cout << Cyan;
+		std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE (Blowfish_Spritz) ]"; std::cout << Reset;
 		print4 (salt_message); std::cout << Reset;
 	}
 	delete [] clear_message;
