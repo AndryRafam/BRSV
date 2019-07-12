4b13ca775d694074f057162c0c573e5f269edfea
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-07-12 10:45:21 +0300

    Update Select2.cpp

--- Print/Select2.cpp
+++ Print/Select2.cpp
@@ -10,9 +10,9 @@ void print2 (uchar *input)
 {
 	Blowfish *B = new(std::nothrow)Blowfish;
 	RC4plus *Rplus = new(std::nothrow)RC4plus;
-		
+
 	uint32_t L, R;
-	
+
 	uchar *cipher = new(std::nothrow)uint8_t[strlen((const char*)input)];
 	int len_RC4plus_key = strlen((const char*)Rplus->RC4plus_key());
 	char *key_temp_RC4plus = new(std::nothrow)char[len_RC4plus_key];
@@ -21,7 +21,7 @@ void print2 (uchar *input)
 	/* Performing RC4plus */
 
 	Rplus->Encrypt ((uchar*)input, key_temp_RC4plus, cipher);
-	
+
 	int len_Blowfish_key = strlen((const char*)B->Blowfish_key());
 	uchar *key_temp_Blowfish = new(std::nothrow)uchar[len_Blowfish_key];
 	memcpy (key_temp_Blowfish, B->Blowfish_key(), len_Blowfish_key);
@@ -30,13 +30,15 @@ void print2 (uchar *input)
 	B->Blowfish_Init (key_temp_Blowfish, len_Blowfish_key);
 	B->crack_text (cipher, &L, &R, strlen((const char*)cipher));
 	B->Blowfish_Encipher (&L, &R);
-	fprintf(stdout,"%08x %08x\n\n\n", R, L); // Print the right part first, then the left part of the encrypted message
+	fprintf(stdout,"\n\n");
+	fprintf(stdout,"\t\t\t\t%08X\n ", R); // Print the right part first, then the left part of the encrypted message
+	fprintf(stdout,"\t\t\t\t%08X\n\n\n ", L);
 
 	delete [ ] key_temp_RC4plus;
 	delete [ ] key_temp_Blowfish;
 	delete [ ] cipher;
 	delete Rplus;
-	delete B;	
+	delete B;
 
 }
 
