f238d0247bb7c35d334542577ce6e4d07709cd61
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-08-16 17:16:21 +0300

    Add files via upload

---
+++ Header/print.h
@@ -0,0 +1,19 @@
+#ifndef _print_h
+#define _print_h
+
+typedef unsigned char uchar;
+
+void print1 (uchar *input);
+void print2 (uchar *input);
+void print3 (uchar *input);
+void print4 (uchar *input);
+void print5 (uchar *input);
+void print6 (uchar *input);
+void print7 (uchar *input);
+void print8 (uchar *input);
+void print9 (uchar *input);
+void print10 (uchar *input);
+void about ();
+uchar *Salt();
+
+#endif
