038724b7be25482c491d6ef0ae4828c530579771
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-03-01 00:18:22 +0300

    Update Main.cpp

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,4 +1,4 @@
-/* Andry RAFAM ANDRIANJAFY - CERBERUS
+/* Andry RAFAM ANDRIANJAFY - Epsilon
 
 OS : Ubuntu 16.04 LTS
 
@@ -28,12 +28,12 @@ int main (int argc, char **args) {
 	uchar *salt_message = new(std::nothrow)uchar[16+strlen((const char*)args[1])];
 	about();
 	std::cout << Yellow << "\n\n\t\t [ Clear message ] → " << Reset << args[1] << std::endl;
-	
+
 	/* Intermediate Stage */
 	std::random_shuffle(args[1],args[1]+strlen((const char*)args[1])); // Shuffle the clear message
 	OTP ((uchar*)args[1]);
 	strcpy((char*)salt_message,(char*)OTP((uchar*)args[1]));
-	strcat((char*)salt_message,(char*)Salt()); 
+	strcat((char*)salt_message,(char*)Salt());
 	std::random_shuffle(salt_message,salt_message+strlen((const char*)salt_message)); // Shuffle the salt message
 	std::cout << "\n\n\t\t << INTERMEDIATE STAGE ... COMPLETED >> " << std::endl;
 
@@ -65,27 +65,27 @@ int main (int argc, char **args) {
 			std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE ] → "; std::cout << Reset;
 			print5 (salt_message); std::cout << Reset;
 			break;
-		
+
 		case 6:
 			std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE ] → "; std::cout << Reset;
 			print6 (salt_message); std::cout << Reset;
 			break;
-			
+
 		case 7:
 			std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE ] → "; std::cout << Reset;
 			print7 (salt_message); std::cout << Reset;
 			break;
-			
+
 		case 8:
 			std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE ] → "; std::cout << Reset;
 			print8 (salt_message); std::cout << Reset;
 			break;
-			
+
 		case 9:
 			std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE ] → "; std::cout << Reset;
 			print9 (salt_message); std::cout << Reset;
 			break;
-			
+
 		case 10:
 			std::cout << Green << "\n\n\t\t [ ENCRYPTED MESSAGE ] → "; std::cout << Reset;
 			print10 (salt_message); std::cout << Reset;
